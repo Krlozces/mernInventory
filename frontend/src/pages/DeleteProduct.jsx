b78974383613261fefb2ee21b3958a2c06e36eaf
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-04-29 11:23:32 -0500

    notistack added

--- frontend/src/pages/DeleteProduct.jsx
+++ frontend/src/pages/DeleteProduct.jsx
@@ -3,23 +3,27 @@ import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
 
 export default function DeleteProduct() {
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
     const { id } = useParams();
-
+    const  { enqueueSnackbar } = useSnackbar();
+    
     const handleDeleteProduct = () => {
         setLoading(true);
         axios
             .delete(`http://localhost:5555/products/${id}`)
             .then(() => {
                 setLoading(false);
+                enqueueSnackbar('Product deleted successfully', { variant: 'success' });
                 navigate('/');
             })
             .catch((error) => {
                 setLoading(false);
-                alert('An error happened. Please check the console.');
+                // alert('An error happened. Please check the console.');
+                enqueueSnackbar('Error', { variant: 'error' })
                 console.log(error);
             });
     };
